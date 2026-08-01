5a8642c13d6bd99a963031d79ed7137d58831d00
GuGuLiuSandao <fzuhyh@foxmail.com>
2026-08-02 00:15:46 +0800

    test: probe docs label on product change

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,6 +1,7 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+// Classification probe: a documentation label must not downgrade a product-path change.
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
